8f529e44856f043f2890b15349915de8083b2cb4
pje8740 <pje8740@cu.ac.kr>
2025-10-20 23:07:26 +0900

    데이터 통신,RTK 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
+import { fetchMultiplePokemonById } from "./RTK/thunk";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const pokemonData = useSelector((state) => state.pokemon);
+  console.log(pokemonData);
+  useEffect(() => {
+    dispatch(fetchMultiplePokemonById(151));
+  }, []);
   return <></>;
 }
 
