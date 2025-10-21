defa1f4a21014ac78909f9769918e35aa5a37baa
pje8740 <pje8740@cu.ac.kr>
2025-10-21 11:34:13 +0900

    디테일 페이지

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,21 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectPokemonById } from "../RTK/selector";
+
 export default function Detail() {
-  return <div>Detail</div>;
+  const { pokemonId } = useParams();
+  const pokemon = useSelector(selectPokemonById(Number(pokemonId)));
+  console.log(Number(pokemonId));
+  return (
+    <div
+      className="flex flex-col justify-center items-center 
+    border border-[gray] p-[30px] rounded-[10px]"
+    >
+      <div className="text-[28px] mb-[10px]">{pokemon.name}</div>
+      <div className="whitespace-pre-wrap text-center">
+        {pokemon.description}
+      </div>
+      <img className="w-[200px]" scr={pokemon.front} />
+    </div>
+  );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,11 +2,11 @@ import { useSelector } from "react-redux";
 import { Card } from "../component/Card";
 
 export default function Main() {
-  const pokemonData = useSelector((state) => state.pokemon);
+  const pokemonData = useSelector((state) => state.pokemon.data);
 
   return (
     <>
-      {pokemonData.data.map((el) => (
+      {pokemonData.map((el) => (
         <Card key={el.id} pokemon={el} />
       ))}
     </>
